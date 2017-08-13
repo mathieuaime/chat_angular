7d8a2e1e38ba2da30c621c161c6174bd8ab6ed04
Mathieu Aimé <aime.mathieu1@gmail.com>
2017-08-13 18:28:32 +0200

    feature(front) message in arrays + add message without content

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  username = "Mathieu";
+
+  messageToSend = '';
+
+  messages = [
+    {
+      name: "Mathieu",
+      timestamp:"1374745445627",
+      content: "Salut !"
+    },
+    {
+      name: "Florian",
+      timestamp:"1474745445627",
+      content: "Salut !"
+    }
+  ]
+
+  public saveMessage() {
+    this.messages.push({
+      name: this.username,
+      timestamp: Date.now().toString(),
+      content: "Hello"
+    })
+  }
 }
